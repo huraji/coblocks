2fb740f269473f0e4ddd1041d81efbcf3f86fb14
Rich Tabor <hi@richtabor.com>
2019-10-10 08:29:06 -0400

    Add help to the ToggleControl

--- src/blocks/logos/inspector.js
+++ src/blocks/logos/inspector.js
@@ -2,24 +2,40 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
 
-const Inspector = props => {
-	return (
-		<InspectorControls>
-			<PanelBody
-				title={ __( 'Logos Settings' ) }
-			>
-				<ToggleControl
-					label={ __( 'Black & White' ) }
-					help={ __( 'Toggle to add a black and white filter.' ) }
-					checked={ props.attributes.grayscale }
-					onChange={ () => props.setAttributes( { grayscale: ! props.attributes.grayscale } ) }
-				/>
-			</PanelBody>
-		</InspectorControls>
-	);
-};
+class Inspector extends Component {
+	getfreeScrollHelp( checked ) {
+		return checked ? __( 'Black and white filtering enabled.' ) : __( 'Toggle to add a black and white filter.' );
+	}
+
+	render() {
+		const {
+			setAttributes,
+			attributes,
+		} = this.props;
+
+		const {
+			grayscale,
+		} = attributes;
+
+		return (
+			<InspectorControls>
+				<PanelBody
+					title={ __( 'Logos Settings' ) }
+				>
+					<ToggleControl
+						label={ __( 'Black & White' ) }
+						help={ this.getfreeScrollHelp }
+						checked={ grayscale }
+						onChange={ () => setAttributes( { grayscale: ! grayscale } ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		);
+	}
+}
 
 export default Inspector;

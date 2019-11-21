165abb0210337f46ffac116d7e0f7794a47d66b5
AnthonyLedesma <anthonymledesma@gmail.com>
2019-11-21 14:50:26 -0700

    some phpcs

--- uninstall.php
+++ uninstall.php
@@ -10,25 +10,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// if uninstall.php is not called by WordPress, die
-if (!defined('WP_UNINSTALL_PLUGIN')) {
-    die;
+// if uninstall.php is not called by WordPress, die.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
 }
 
-$__options = array(
+$coblocks_options = array(
 	'coblocks_settings_api',
+	'coblocks_date_installed',
+	'coblocks_plugin_feedback_activation_date',
+	'coblocks_plugin_feedback_no_bug',
 );
 
-foreach ( $__options as $option_name ) {
+foreach ( $coblocks_options as $option_name ) {
 	if ( get_option( $option_name ) === true ) {
-        if ( function_exists( 'is_multisite' ) && is_multisite() ) { 
-            // for site options in Multisite
-            delete_site_option( $option_name );
-        } else {
-                delete_option( $option_name );
-        }
-    }
+		if ( function_exists( 'is_multisite' ) && is_multisite() ) {
+			// for site options in Multisite.
+			delete_site_option( $option_name );
+		} else {
+			delete_option( $option_name );
+		}
+	}
 }
 
-unset( $__options );
-?>
+unset( $coblocks_options );
